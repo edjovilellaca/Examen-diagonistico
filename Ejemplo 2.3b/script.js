d82a85fdcbadf3bde06a359ea737eb776a96a466
edjovilellaca <edjovilellaca@ittepic.edu.mx>
2024-08-27 22:10:34 -0700

    Delete Ejemplo 2.3b directory

--- Ejemplo 2.3b/script.js
+++
@@ -1,8 +0,0 @@
-const titulo = document.getElementById('titulo');
-const boton = document.getElementById('cambiarTexto');
-
-function cambiarTexto() {
-    titulo.textContent = 'Texto cambiado';
-}
-
-boton.addEventListener('click', cambiarTexto);
